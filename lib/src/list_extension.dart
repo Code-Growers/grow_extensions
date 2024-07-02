de9779c7dc907e5a50568551d633101984c16603
Adam Kvasnička <adam.kvasnicka.dmd@gmail.com>
2024-07-02 15:47:38 +0200

    feat: update uniqBy

--- lib/src/list_extension.dart
+++ lib/src/list_extension.dart
@@ -16,17 +16,11 @@ extension FebuListExtension<T> on List<T> {
   /// ```dart
   /// <Cat>[Cat(name: 'Garfield'), Cat(name: 'Garfield')].uniqBy((Cat cat) => cat.name) // <Cat>[Cat(name: 'Garfield')]
   /// ```
-  List<T> uniqBy<T, Y>(Y Function(T) onMap) {
-    final List<Y> uniqueIdentifies =
-        map((dynamic entity) => onMap(entity as T)).toSet().toList();
-    return uniqueIdentifies.map((Y identifier) {
-      try {
-        return firstWhere(
-            (dynamic element) => onMap(element as T) == identifier);
-      } catch (_) {
-        return null;
-      }
-    }).toList() as List<T>;
+  List<T> uniqBy<T, Y>([Y Function(T element)? id, bool inplace = true]) {
+    final ids = <dynamic>{};
+    final List<T> list = inplace ? this as List<T> : List<T>.from(this);
+    list.retainWhere((x) => ids.add(id != null ? id(x) : x as Y));
+    return list;
   }
 
   /// when using simple structures create unique list using toSet method
@@ -35,17 +29,17 @@ extension FebuListExtension<T> on List<T> {
   /// ```
   List<T> get simpleUniq => toSet().toList();
 
-  /// check if element is last in list 
+  /// check if element is last in list
   /// ```dart
-  /// <int>[1, 2, 3].isLast(1) // false 
-  /// <int>[1, 2, 3].isLast(3) // true 
+  /// <int>[1, 2, 3].isLast(1) // false
+  /// <int>[1, 2, 3].isLast(3) // true
   /// ```
   bool isLast(T element) => indexOf(element) == length - 1;
 
-  /// check if element is first in list 
+  /// check if element is first in list
   /// ```dart
-  /// <int>[1, 2, 3].isFirst(1) // true 
-  /// <int>[1, 2, 3].isFirst(3) // false 
+  /// <int>[1, 2, 3].isFirst(1) // true
+  /// <int>[1, 2, 3].isFirst(3) // false
   /// ```
   bool isFirst(T element) => indexOf(element) == 0;
 }
